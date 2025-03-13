3223bc104cf3a89894003f093b3690512a6a9b39
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-03-13 19:38:05 +0000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 import { Box, Flex, Text, Link, HStack } from '@chakra-ui/react';
 
+/**
+ * The Footer component provides a simple navigational area
+ * (e.g. About, Contact links) and displays a copyright
+ * notice at the bottom of the page.
+ *
+ * @returns {JSX.Element} A responsive footer section.
+ */
 const Footer = () => {
   return (
-    <Box as="footer" bg="gray.900" color="white" py={4} width="100%">
-      <Flex direction="column" align="center">
+    <Box
+      as="footer"
+      bg="gray.900"
+      color="white"
+      py={6}          
+      w="100%"
+      mt={8}
+      boxShadow="inner"
+    >
+      <Flex direction="column" align="center" justify="center">
+        {/* Navigation Links */}
         <HStack spacing={8} mb={4}>
-          <Link href="/about" _hover={{ color: "teal.300" }}>
+          <Link
+            href="/about"
+            fontSize="md"
+            fontWeight="medium"
+            _hover={{ color: "teal.300", textDecoration: "underline" }}
+          >
             About
           </Link>
-          <Link href="/contact" _hover={{ color: "teal.300" }}>
+          <Link
+            href="/contact"
+            fontSize="md"
+            fontWeight="medium"
+            _hover={{ color: "teal.300", textDecoration: "underline" }}
+          >
             Contact
           </Link>
         </HStack>
+
+        {/* Copyright */}
         <Text fontSize="sm" color="gray.500">
           &copy; {new Date().getFullYear()} My Personal Photo Gallery. All Rights Reserved.
         </Text>
