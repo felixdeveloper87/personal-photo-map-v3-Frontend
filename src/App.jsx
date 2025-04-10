79c68f6f223ea655fb0bf682c3691ac8f168117b
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-04-10 23:58:23 +0100

    adminLogin

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/admin-login" element={<AdminLogin />} />
               <Route path="/register" element={<Register />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
