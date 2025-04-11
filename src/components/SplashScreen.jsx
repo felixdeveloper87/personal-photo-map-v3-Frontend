917de96c99c6fc56f51f6645a60f4c103696d72f
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-04-11 22:52:11 +0100

    improving

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -2,6 +2,7 @@
 import { Box, Center, Image, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
+import bgImage from "../assets/background.jpg";
 
 const SplashScreen = ({ onFinish }) => {
   const [fadeOut, setFadeOut] = useState(false);
@@ -23,16 +24,29 @@ const SplashScreen = ({ onFinish }) => {
       left={0}
       w="100vw"
       h="100vh"
-      bg="white"
       zIndex={9999}
       opacity={fadeOut ? 0 : 1}
       transition="opacity 1s ease"
+      bgImage={`url(${bgImage})`}
+      bgSize="cover"
+      bgPosition="center"
+      bgRepeat="no-repeat"
     >
-      <Center h="100%">
+      {/* Overlay escuro pra dar contraste */}
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        w="100%"
+        h="100%"
+        bg="blackAlpha.700"
+      />
+
+      <Center h="100%" position="relative" zIndex={1}>
         <Box textAlign="center">
-        <Image src={logo} alt="Photomap Logo" boxSize="100px" mb={4} />
-          <Text fontSize="3xl" fontWeight="bold" color="gray.700">
-            Photomap
+          <Image src={logo} alt="Photomap Logo" boxSize="100px" mb={4} />
+          <Text fontSize="3xl" fontWeight="bold" color="white">
+            Welcome to Photomap
           </Text>
         </Box>
       </Center>
