62590732cf6041b2efd817bb2919c433f718e1b4
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-04-12 00:41:12 +0100

    welcome2

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,16 +1,39 @@
 // src/pages/Welcome.jsx
 import { Box, Button, Heading, Stack, Text, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import bgImage from "../assets/continents.jpg";
 
 const Welcome = () => {
   const navigate = useNavigate();
 
   return (
-    <Flex bg="blue.50" minH="100vh" justify="center" pt={20} px={6}>
-      <Box textAlign="center" maxW="xl">
+    <Flex
+      minH="100vh"
+      justify="center"
+      pt={20}
+      px={6}
+      bgImage={`url(${bgImage})`}
+      bgSize="cover"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      position="relative"
+    >
+      {/* Overlay escuro para melhor contraste do texto */}
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        w="100%"
+        h="100%"
+        bg="blackAlpha.700"
+        zIndex={0}
+      />
+
+      {/* Conteúdo acima da imagem */}
+      <Box textAlign="center" maxW="xl" position="relative" zIndex={1} color="white">
         <Heading mb={4}>Welcome to Photomap 🌍</Heading>
         <Text fontSize="lg" mb={6}>
-          Explore new ideas, cultures and history around the world. Get real time information about countries, register your memories and explore our beatiful planet.
+          Explore new ideas, cultures and history around the world. Get real time information about countries, register your memories and explore our beautiful planet.
         </Text>
 
         <Stack direction={["column", "row"]} spacing={4} justify="center">
