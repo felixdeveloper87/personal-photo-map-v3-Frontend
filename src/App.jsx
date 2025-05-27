af79d0331d0a9a8b58583ca78218923b903ed026
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-05-27 12:32:42 +0100

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@
  * 
  */
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Box, Flex } from '@chakra-ui/react';
 import Home from './pages/Home';
 import Header from './components/Header';
