5879dd2b0b2b016f3c2c0b17e9c5a837eb607b15
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-05-27 12:52:09 +0100

    go

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/leaflet.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import theme from './theme';
@@ -42,7 +42,7 @@ function AppWithQueryClient() {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider theme={theme}> {/* Aplique o tema aqui */}
+      <ChakraProvider theme={theme}> 
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <AppWithQueryClient />
       </ChakraProvider>
