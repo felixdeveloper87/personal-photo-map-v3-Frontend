11ca2ddd17f9fab53974835b94546739f73ae07c
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-06-02 02:28:37 +0100

    gd

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/leaflet.css';
+import './styles/tooltip.css';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
